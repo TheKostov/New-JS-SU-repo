79acb30e057af994ea0d28fe593b84370a1cb653
TheKostov <dimitar.k.kostov@gmail.com>
2023-01-28 10:20:07 +0200

    we're back

---
+++ Excercises/Syntax, Functions and Statements/timeToWalk.js
@@ -0,0 +1,15 @@
+function solve(stepsNumber, stepLengthMeters, speedKmH){
+    let distance = stepsNumber * stepLengthMeters;
+    let speedMPerS = speedKmH / 3.6;
+    let rest = Math.floor(distance / 500) * 60; 
+    let time = distance / speedMPerS + rest;
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.round(time % 60);
+
+    console.log(`${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`)
+}
+
+
+solve(4000, 0.60, 5);
+solve(2564, 0.70, 5.5);
