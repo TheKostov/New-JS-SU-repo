dd7d7c96cecdd88117ae12bc32af964b96d6fec0
TheKostov <dimitar.k.kostov@gmail.com>
2023-02-07 18:57:11 +0200

    join opp

---
+++ Excercises/Arrays and Nested Arrays/delimeterPrint.js
@@ -0,0 +1,10 @@
+function solve(inputArr, delim){
+    let printArr = inputArr.join(delim);
+    console.log(printArr);
+}
+
+
+
+
+solve(['One', 'Two', 'Three', 'Four', 'Five'], '-');
+solve(['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_');
