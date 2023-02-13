3ced8c13379330d8676dca7a71e6d429a9ee4799
TheKostov <dimitar.k.kostov@gmail.com>
2023-02-13 12:22:06 +0200

    this will take some getting used to

---
+++ Lectures/Objects and Composition/townPopulationAlt.js
@@ -0,0 +1,29 @@
+function townPopulationAltSyntax(inputArr){
+    const result = {};
+
+    for (let line of inputArr){
+        const [townName, townPopulation] = line.split(' <->');
+
+       if (townName in result == false){
+            result[townName] = 0;
+       }
+
+       result[townName] += Number(townPopulation);
+    }
+    console.log(Object
+        .entries(result)
+        .map(([townName, townPopulation]) => `${townName} : ${townPopulation}`)
+        .join('\n'));
+}
+
+townPopulationAltSyntax(['Sofia <-> 1200000',
+                'Montana <-> 20000',
+                'New York <-> 10000000',
+                'Washington <-> 2345000',
+                'Las Vegas <-> 1000000']);
+
+townPopulationAltSyntax(['Istanbul <-> 100000',
+                'Honk Kong <-> 2100004',
+                'Jerusalem <-> 2352344',
+                'Mexico City <-> 23401925',
+                'Istanbul <-> 1000']);
