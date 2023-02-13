5a4b498e78c4fbd9049b172051ab20cd47994964
TheKostov <dimitar.k.kostov@gmail.com>
2023-02-13 11:11:39 +0200

    gm

---
+++ Lectures/Objects and Composition/cityRecord.js
@@ -0,0 +1,22 @@
+function cityRecord (name, population, treasury){
+    const city = {
+        name,
+        population,
+        treasury
+    }
+    console.log(city);
+}
+
+
+
+cityRecord('Tortuga',
+
+7000,
+
+15000);
+
+cityRecord('Santo Domingo',
+
+12000,
+
+23500)
---
+++ Lectures/Objects and Composition/testing.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Peter',
+    age: 25,
+    lastName: 'Johnson',
+};
+
+
+const {name, lastName} = person;
+
+console.log(name, lastName);
+
+
