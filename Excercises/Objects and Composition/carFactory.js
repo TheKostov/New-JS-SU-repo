bf9891d2574162929b4ed08ba9d038207925e892
TheKostov <dimitar.k.kostov@gmail.com>
2023-02-14 13:54:10 +0200

    new day

---
+++ Excercises/Objects and Composition/carFactory.js
@@ -0,0 +1,48 @@
+function solve({ model, power, color, carriage, wheelsize }){
+    const engines = {
+        small : {power : 90, volume : 1800},
+        medium : {power : 120, volume : 2400},
+        monster : {power : 200, volume : 3500},
+    }
+
+    let engine = {};
+    if (power <= 90){
+        engine = engines.small;
+    } else if (power <= 120){
+        engine = engines.medium;
+    } else {    
+        engine = engines.monster;
+    }
+    
+    const newWheelSize = wheelsize % 2 === 0 ? --wheelsize : wheelsize;
+
+    console.table(result);
+    return result = {
+        model,
+        engine,
+        color,
+        carriage : {
+            type : carriage,
+            color,
+        },
+        wheelsize : Array(4).fill(newWheelSize),
+    }
+}
+
+
+
+
+
+solve({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+})
+
+
+//{ model: 'VW Golf II', 
+//engine: { power: 90, volume: 1800 }, 
+//carriage: { type: 'hatchback', color: 'blue' }, 
+//wheels: [13, 13, 13, 13] }
