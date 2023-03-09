9d897aae7167698d68a589a32fc4372648b39a20
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-09 11:12:46 +0200

    progress

--- Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    // read input value
+    const query = document.querySelector('input[name="email"]').value;
+    // get all rows
+    const rows = document.querySelectorAll('#customers tbody tr');
+    const rowsArray = Array.from(rows);
+    // iterate rows and compare content to input
+    for (let row of rowsArray){
+        const col = row.children()[1];
+        
+        if (col.textContent == query){
+            // remove matching row
+            row.remove();
+        }
+    }
 }
