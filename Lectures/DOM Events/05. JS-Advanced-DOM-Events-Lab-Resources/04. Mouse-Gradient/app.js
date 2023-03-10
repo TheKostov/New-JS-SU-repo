9bb698fa323c83aeb3e747ab0d753c0b41f15314
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-10 10:18:24 +0200

    final

--- Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,6 +1,6 @@
 function attachGradientEvents() {
     const gradient = document.getElementById('gradient');
-    gradient.addEventListener('click', onClick);
+    gradient.addEventListener('mouseover', onClick);
     function onClick(event){
         const x = event.offsetX;
         const percent = Math.floor(x / 300 * 100);
