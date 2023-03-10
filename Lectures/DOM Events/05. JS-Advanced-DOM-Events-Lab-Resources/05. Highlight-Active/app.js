459a158f4db5076d05706bc96717cb5355f38f68
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-10 11:16:12 +0200

    adding class elements to parents

--- Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
+++ Lectures/DOM Events/05. JS-Advanced-DOM-Events-Lab-Resources/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    console.log('TODO:...');
+    const inputs = Array.from(document.querySelectorAll('input'));
+
+    for (let input of inputs) {
+        input.addEventListener('focus', () => {
+            input.parentElement.className = 'focused';
+        });
+        input.addEventListener('blur', () => {
+            input.parentElement.className = 'blurred';
+        });
+    }
 }
