1b600524c88071297bbc4f5f4bd319109df0a786
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-11 13:45:45 +0200

    usdc is depegging lmeow

--- Excercises/DOM Manipulation/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
+++ Excercises/DOM Manipulation/05. JS-Advanced-DOM-Events-Exercise-Resources/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   const parent = document.getElementById('content');
+
+   words.forEach(element => {
+      const div = document.createElement('div');
+      const paragraph = document.createElement('p');
+      paragraph.textContent = element;
+      paragraph.style.display = 'none';
+      div.appendChild(paragraph);
+      div.addEventListener('click', (event) => {
+         event.target.querySelector('p').style.display = 'block';
+      });
+      parent.appendChild(div);
+   });
 }
