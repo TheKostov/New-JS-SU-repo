42045d330872da226e0a457e13848491263d0a5a
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-24 15:18:00 +0200

    another fibunacci

--- Excercises/Advanced Functions/argumentInfo.js
+++ Excercises/Advanced Functions/argumentInfo.js
@@ -3,9 +3,13 @@ function solve() {
     const count = new Map();
     arg.forEach((element) => {
         const type = typeof element;
-        console.log(`${type}: ${element}`)
-    })
-
+        console.log(`${type}: ${element}`);
+        if (!count.has(type)) {
+            count.set(type, 0)
+        }
+        count.set(type, count.get(type) + 1)
+    });
+    Array.from(count.entries()).sort((a, b) => b[1] - a[1]).forEach((element) => console.log(`${element[0]} = ${element[1]}`));
 
 }
 
