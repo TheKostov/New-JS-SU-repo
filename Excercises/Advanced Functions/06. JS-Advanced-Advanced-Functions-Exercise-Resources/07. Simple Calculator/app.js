e78c8f865226debe09864620a3088e037c622eb3
TheKostov <dimitar.k.kostov@gmail.com>
2023-03-25 16:46:29 +0200

    m

--- Excercises/Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
+++ Excercises/Advanced Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
@@ -1,5 +1,21 @@
 function calculator() {
-    // TODO:
+    let sumElement1;
+    let sumElement2;
+    let resultElement;
+
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            sumElement1 = document.querySelector(selector1);
+            sumElement2 = document.querySelector(selector2);
+            resultSelector = document.querySelector(resultElement);
+        },
+        add:() => {
+            resultElement.value = +sumElement1.value + +sumElement2.value;
+        },
+        subtract: () =>{
+            resultElement.value = +sumElement1.value - +sumElement2.value;
+        }
+    };
 }
 
 
