489229d259f55c789e6d3de21a4abc4f53279281
TheKostov <dimitar.k.kostov@gmail.com>
2023-04-19 19:10:27 +0300

    m

--- Excercises/Unit Testing/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
+++ Excercises/Unit Testing/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/07.Form-Validation/formValidation.js
@@ -1,3 +1,84 @@
 function validate() {
-    // TODO
+    const companyField = document.getElementById('companyInfo');
+    const companyCheck = document.getElementById('company');
+    const form = document.getElementById('registerForm');
+
+    companyCheck.addEventListener('change', () => {
+        if (companyCheck.checked) {
+            companyField.style.display = 'block'
+        } else {
+            companyField.style.display = 'none'
+        }
+    })
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const userNameImput = document.getElementById('username');
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+        const rePasswordInput = document.getElementById('confirm-password');
+        const companyNumberInput = document.getElementById('companyNumber');
+
+        const validField = document.getElementById('valid');
+
+        const userNamePattern = /^[a-zA-Z0-9]+$/;
+        const passwordPattern = /^[\w]+$/;
+        const emailePattern = /.*@.*\..*/;
+        
+        let usernameIsValid = false;
+        let passwordIsValid = false;
+        let repasswordIsValid = false;
+        let emailIsValid = false;
+        let companyNumberIsValid = false;
+
+        if (
+        userNamePattern.test(userNameImput.value) && 
+        userNameImput.value.length >= 3 && 
+        userNameImput.value.length <= 20
+        ) {
+            usernameIsValid = true;
+        }
+
+        if (emailePattern.test(emailInput.value)) {
+            emailIsValid = true;
+        }
+        
+        if (
+        passwordPattern.test(passwordInput.value) && 
+        passwordInput.value.length >= 5 && 
+        passwordInput.value.length <= 15 && 
+        passwordInput.value == rePasswordInput.value
+        ) {
+            passwordIsValid = true;
+            repasswordIsValid = true;
+        }
+
+        if (companyCheck.checked){
+            if (companyNumberInput.value >= 1000 && companyNumberInput.value <= 9999) {
+                companyNumberIsValid = true;
+            }   
+        }
+        else {
+            companyNumberIsValid = true;
+        }
+        if (usernameIsValid && passwordIsValid && repasswordIsValid && emailIsValid && companyNumberIsValid) {
+            validField.style.display = 'block'
+        }
+        if (!usernameIsValid){
+            userNameImput.style.borderColor = 'red';
+        }
+        if (!passwordIsValid){
+            passwordInput.style.borderColor = 'red'
+        }
+        if (!repasswordIsValid){
+            rePasswordInput.style.borderColor = 'red'
+        }
+        if (!emailIsValid){
+            emailInput.style.borderColor = 'red'
+        }
+        if (!companyNumberIsValid) {
+            companyNumberInput.style.borderColor = 'red';
+        }
+        
+    });
 }
